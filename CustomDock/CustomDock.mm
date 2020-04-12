565ac701cc23e3fdcdf08bf1511aa6feb9c106ec
i0s_tweak3r <djvs23@gmail.com>
2020-04-12 06:51:41 -0400

    Changed imports to modern ones
    Preferences.h is outdated

--- CustomDock/CustomDock.mm
+++ CustomDock/CustomDock.mm
@@ -1,9 +1,15 @@
-#import <Preferences/Preferences.h>
+// old iOS 9 SDK #import <Preferences/Preferences.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSTableCell.h>
+// Modern Preference bundles use these three imports ^^
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 @interface CustomDockListController: PSListController {
 }
+- (void)twitterlink;
+- (void)donate;
 @end
 
 
